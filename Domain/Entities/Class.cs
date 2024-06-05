d6fc1a7688b341edc8e08e1d6710e1bf8e5def0d
Kuldeep Bugalia <duffybugalia@gmail.com>
2024-06-05 13:48:18 +0530

    #Create New Class named Class

---
+++ Domain/Entities/Class.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Class:BaseAuditableEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
